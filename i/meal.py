7e85f5384618012d6ce86fd7ef87fb7d477d175f
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-19 14:17:29 +0000

    Mon, Dec 19, 2022, 2:17 PM +00:00

--- i/meal.py
+++ i/meal.py
@@ -1,13 +1,22 @@
 def main():
-
+    time = input("Enter: ")
+    convert(time)
+    if time > 7 and time < 8:
+        print("breakfast time")
+    elif time > 12 and time < 13:
+        print("lunch time")
+    elif time > 18 and time < 19:
+        print("dinner time")
+    else:
+        x = 4
 
 
 def convert(time):
-hours, minutes = time.split(":")
-mes = minutes/60
-float(mes)
-float(hours)
-return hours + mes
+    hours, minutes = time.split(":")
+    mes = minutes/60
+    float(mes)
+    float(hours)
+    return hours + mes
 
 if __name__ == "__main__":
     main()
