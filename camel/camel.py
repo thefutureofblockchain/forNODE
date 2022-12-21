051429004f5d357114f898de7275e83ca885b983
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-21 15:29:34 +0000

    Wed, Dec 21, 2022, 3:29 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -3,14 +3,14 @@ def main():
 
     for letter in user_input:
         if letter.isupper():
-            predecessor ,partition ,successor = user_input.partition(letter)
+            predecessor ,partition ,successor = user_input.split(sep = letter)
             letter = letter.lower()
             letter = letter.replace(letter,"_"+letter)
             # print(letter)
             user_input = user_input.lower()
 
             #print(letter, end = "")
-            print(predecessor , letter,successor, sep="", end = "")
+            print(predecessor , "_",letter,"_",successor, sep="", end = "")
         else:
             c = 1
             #print(letter, end= "")
