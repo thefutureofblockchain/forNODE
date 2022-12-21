85a78653ece38008cf5357816f2c435c5c2dee15
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-21 14:21:45 +0000

    Wed, Dec 21, 2022, 2:21 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,13 +2,13 @@ def main():
     a = input("Enter eet: ")
 
     for letter in a:
-        lets, me = a.split(isupperr(letter))
-        print(lets , me )
-        '''if letter.isupper():
+        '''lets, me = a.split(isupperr(letter))
+        print(lets , me )'''
+        if letter.isupper():
             lets, me = a.split(letter.isupper)
             letter = letter.lower()
             print(letter, end = "")
         else:
             print(letter, end= "")
-    print()'''
+    print()
 
