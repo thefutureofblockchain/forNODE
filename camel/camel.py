e0980d4bb2cda9989131df6ac80291612839386d
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-21 16:00:33 +0000

    Wed, Dec 21, 2022, 4:00 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -3,7 +3,7 @@ def main():
 
     for letter in user_input:
         if letter.isupper():
-            if user_input.find(letter.isupper) == 1:
+            if user_input.find(al_is(user_input)) == "1":
                 predecessor ,successor = user_input.split(sep = letter)
                 letter = letter.lower()
                 #letter = letter.replace(letter,"_"+letter)
@@ -25,5 +25,11 @@ def main():
             c = 1
             #print(letter, end= "")
     print()
+
+def al_is(inpt):
+    if inpt.find("A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J" or "K" or "L" or "M" or "N" or "O" or "P" or "Q" or "S" or "T" or "U" or "V" or "W" or "X" or "Y" or "Z" ) == 1:
+     return 1
+    else:
+     return 2
 
 main()
