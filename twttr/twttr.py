887d3152c4ed14e9a15b933b77062af6a2056545
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-21 14:05:35 +0000

    Wed, Dec 21, 2022, 2:05 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,1 @@
-a = input("Enter EETODK: ")
-for letter in a:
-    letter = letter.lower()
-    if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
-        continue
-    print(letter)
+'banana'.isupper()
