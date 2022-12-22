2a9fb2c9f4cae78202f49d9607a61f9a069673da
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-22 14:04:10 +0000

    Thu, Dec 22, 2022, 2:04 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,7 +2,7 @@ amtdue = 50
 print("Amount Due:" , amtdue  )
 coins = int(input("Insert Coin : "))
 if coins == 25 or coins == 10 or coins == 5:
-    while coins <= 50:
+    while coins <= amtdue:
         amtdue = 50 - coins
         print("Amount due:" , amtdue)
         coins = int(input("Insert Coin : "))
