3c114bf5fd41833005ac493c137f30a5fd5e5ee4
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-22 14:42:38 +0000

    Thu, Dec 22, 2022, 2:42 PM +00:00

--- coke/cke.py
+++ coke/cke.py
@@ -1,14 +1,16 @@
 while True:
-    n = int(input("Insert Coin: "))
-    if n != 25 or n != 10 or n != 5:
-        amtdue = 50 - n
-        print("Amount due:" , amtdue)
-        x = int(input("Insert Coin: "))
-        amtdue = amtdue - n+x
-        print("Amount due: ", amtdue)
-        y = int(input("Insert Coin: "))
-        amtdue = amtdue - y
-        print("Amount due: ", amtdue)
+    n == 0:
+    if n == 0:
+        n = int(input("Insert Coin: "))
+            if n != 25 or n != 10 or n != 5:
+                amtdue = 50 - n
+                print("Amount due:" , amtdue)
+                x = int(input("Insert Coin: "))
+                amtdue = amtdue - n+x
+                print("Amount due: ", amtdue)
+                y = int(input("Insert Coin: "))
+                amtdue = amtdue - y
+                print("Amount due: ", amtdue)
 '''sum = n-50
 print("Change = ", sum)
 '''
