72694916e15947d972e2eb84b4d57b84b47ac2b2
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-22 15:04:54 +0000

    Thu, Dec 22, 2022, 3:04 PM +00:00

--- coke/cke.py
+++ coke/cke.py
@@ -2,14 +2,14 @@ given = 0
 to_get = 0
 amtdue = 50
 while True:
-    coins = int(input("Insert Coin"))
+    coins = int(input("Insert Coin: "))
     if coins == 25 or coins == 10 or coins == 5:
         given = to_get + coins
         amtdue = amtdue - given
     if amtdue > 0:
-        print("amount due is ", amtdue)
+        print("amount due is: ", amtdue)
     elif amtdue <= 0:
         change = amtdue*-1
-        print("channge is", change)
+        print("channge is: ", change)
 
 
