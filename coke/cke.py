2337386da216816d7320938ff21708aa57ac82a1
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-22 14:23:23 +0000

    Thu, Dec 22, 2022, 2:23 PM +00:00

--- coke/cke.py
+++ coke/cke.py
@@ -3,8 +3,9 @@ while True:
     if n > 50:
         break
     elif n == 25 or n == 10 or n == 5:
-        amtdue = 50 - coins
+        amtdue = 50 - n
         print("Amount due:" , amtdue)
+        
 sum = n-50
 print("Change = ", sum)
 
