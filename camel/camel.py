b9c1ac8e22552bc4f775c39052c0ea5fe1ba3d02
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-22 13:30:26 +0000

    Thu, Dec 22, 2022, 1:30 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,36 +1,5 @@
-def main():
-    user_input = input("Enter it: ")
-
-    for letter in user_input:
-        if letter.isupper():
-            if al_is(user_input) == 1:
-                predecessor ,successor = user_input.split(sep = letter)
-                letter = letter.lower()
-                #letter = letter.replace(letter,"_"+letter)
-                # print(letter)
-                user_input = user_input.lower()
-
-                #print(letter, end = "")
-                print(predecessor , "_",letter,successor, sep="", end = "")
-            else:
-                a, predecessor ,successor = user_input.split(sep = letter)
-                letter = letter.lower()
-                #letter = letter.replace(letter,"_"+letter)
-                # print(letter)
-                user_input = user_input.lower()
-
-                #print(letter, end = "")
-                print(a, "_" , predecessor , "_",letter,successor, sep="", end = "")
-        else:
-            c = 1
-            #print(letter, end= "")
-    print()
-
-def al_is(inpt):
-    if inpt.find("A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J" or "K" or "L" or "M" or "N" or "O" or "P" or "Q" or "S" or "T" or "U" or "V" or "W" or "X" or "Y" or "Z" ) == 1:
-     return 1
-     print("i")
-    else:
-     return 2
-
-main()
+a = input("Enter: ")
+for letter in a:
+    if letter.isupper():
+        a = a.replace(letter, "_" + letter.lower())
+print(a)
--- camel/cmel.py
+++
@@ -1,5 +0,0 @@
-a = input("Enter: ")
-for letter in a:
-    if letter.isupper():
-        a = a.replace(letter, "_" + letter.lower())
-print(a)
