9fe915cb01aa88ee9db3b673d03542535ec4f824
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-22 13:43:32 +0000

    Thu, Dec 22, 2022, 1:43 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,1 +1,6 @@
-q = input("")
+q = input("Enter: ")
+for letter in q:
+    if letter =="a" or letter =="e" or letter == "i" or letter == "o" or letter == "u" or letter =="A"or letter =="E"or letter =="I"or letter =="O"or letter =="U" :
+        continue
+    print(letter, end = "")
+print()
