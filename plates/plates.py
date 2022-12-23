b79b0ed41f968c1701652a2795d006b1ccc4f4d9
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-23 14:24:10 +0000

    Fri, Dec 23, 2022, 2:24 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,6 +7,11 @@ def main():
 
 
 def is_valid(s):
-    
+    if is_let(s) == True and is_6(s) == True and is_num(s) == True and is_period(s) == True:
+        return True
+    else:
+        return False
+
+def is_let(a)
 
 main()
