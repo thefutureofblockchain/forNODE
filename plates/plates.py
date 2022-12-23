0264b18fc42688bbdadfc2d2d48180d806450a73
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-23 14:36:33 +0000

    Fri, Dec 23, 2022, 2:36 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,18 +6,33 @@ def main():
         print("Invalid")
 
 
+
 def is_valid(s):
     if is_let(s) == True and is_6(s) == True and is_num(s) == True and is_period(s) == True:
         return True
     else:
         return False
-
 def is_let(a):
-    
+    ba = a[0:2]
+    if ba.isalpha() == True:
+        return True
+    else:
+        return False
 
 def is_6(b):
+    if len(b) <= 6 and len(b) <=2 :
+        return True
+    else:
+        return False
 
-def is_num(C):
+def is_num(c):
+
+
 def is_period(d):
+        if d.isalnum() == True:
+            return True
+        else:
+            return False
+
 
 main()
