30c99c7dea1b54f8d973bed71ab98bd6ca3117ae
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-23 15:10:26 +0000

    Fri, Dec 23, 2022, 3:10 PM +00:00

--- plates/t.py
+++ plates/t.py
@@ -1,6 +1,9 @@
 c = input("Enterr")
 for letter in c:
     if letter.isdigit():
-       qa, b = c.split(sep = letter)
-       print(qa)
-       print(b)
+       qa, b, d = c.partition(letter)
+       print(qa, b, d)
+       if d.isdigit() or d == 0:
+        print("yes")
+       else:
+        print("no")
