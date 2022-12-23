363de041d3bf1b0f7f6bd50ccf3a2d54a75bdbc5
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-23 14:46:48 +0000

    Fri, Dec 23, 2022, 2:46 PM +00:00

--- plates/test.py
+++ plates/test.py
@@ -5,13 +5,18 @@ def main():
     else:
         print("Invalid")
 def is_valid(s):
-    if is_let(s) == True:
+    if is_let(s) == True and is_6(s) == True:
         return True
     else:
         return False
 def is_let(a):
     ba = a[0:2]
     if ba.isalpha() == True:
+        return True
+    else:
+        return False
+def is_6(b):
+    if len(b) <= 6 and len(b) <=2 :
         return True
     else:
         return False
