33497cb342742f39d194e4b1a0bd635d339a6215
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-24 11:37:21 +0000

    Sat, Dec 24, 2022, 11:37 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,4 +1,12 @@
 def main():
-    x = float(input("x: " ))
-    y = float(input("y: "))
-    
+    xy = get_frac()
+def get_frac()
+    while True:
+        try:
+            x = int(input("What's x?"))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    return x
+
