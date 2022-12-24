34cc61766ad7d820a290682cf14f502af1b91eb2
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-24 18:21:55 +0000

    Sat, Dec 24, 2022, 6:21 PM +00:00

---
+++ outdated/outd.py
@@ -0,0 +1,5 @@
+def is_char(i):
+    if i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i ==7 or i == 8 or i == 9:
+        return True
+    else:
+        return False
--- outdated/outdated.py
+++ outdated/outdated.py
@@ -46,13 +46,20 @@ def conv(n):
                     month, day, year = n.split()
                     month = mon.index(month)
                     month = int(month)
-                    '''month = month:02'''
-                    day = 'day:02'
+                    if is_char(month) == True:
+                        month = ("0" + month)
+                    if is_char(day) == True:
+                         day = ("0" + day)
                     print(year,"-",month, "-",day,sep="")
             if month > 12 or day > 31:
                     raise ValueError
 
     except (ValueError,KeyError):
         pass
+def is_char(i):
+    if i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i ==7 or i == 8 or i == 9:
+        return True
+    else:
+        return False
 
 main()
