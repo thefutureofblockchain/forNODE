9331f2bf9949326a91f984e7261c10e52e278f38
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-25 15:14:45 +0000

    Sun, Dec 25, 2022, 3:14 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,47 +1,46 @@
 
-#while True:
-try:
-            a = input("enter: ")
-            mon = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-            ]
+while True:
+        try:
+                a = input("enter: ")
+                mon = [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+                ]
 
-            if "/" in a:
-                    month, day, year = a.split(sep = "/")
-                    month = int(month)
-                    day = int(day)
-                    year = int(year)
-                    if int(month) > 12 or int(day) > 31:
-                        raise ValueError
+                if "/" in a:
+                        month, day, year = a.split(sep = "/")
+                        month = int(month)
+                        day = int(day)
+                        year = int(year)
+                        if int(month) > 12 or int(day) > 31:
+                                raise ValueError
+                        break
 
-            elif "," in a:
+                elif "," in a:
 
-                    a = a.replace(",", " ")
-                    day,month,year = a.split()
-                    day = day.strip()
-                    month = month.strip()
-                    year = year.strip()
-                    month = mon.index(month)
-                    month = int(month)
-                    if int(month) > 12 or int(day) > 31:
-                            raise ValueError
+                        a = a.replace(",", " ")
+                        day,month,year = a.split()
+                        day = day.strip()
+                        month = month.strip()
+                        year = year.strip()
+                        month = mon.index(month)
+                        month = int(month)
+                        if int(month) > 12 or int(day) > 31:
+                                raise ValueError
+                        break
+        except (ValueError,KeyError):
+                #print()
+                continue
 
-
-            print(f"{year}-{month:02}-{day:02}")
-
-except (ValueError,KeyError):
-        print("there was an error")
-        pass
-
+print(f"{year}-{month:02}-{day:02}")
 
