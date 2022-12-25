f49c4d7a92ec0e50285ab73e362f7182b431508f
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-25 09:32:32 +0000

    Sun, Dec 25, 2022, 9:32 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,22 +1,7 @@
-def main():
-    mon = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-    ]
-    a = input("enterr")
-    c = conv(a)
-def conv(n):
-    try:
+
+#while True:
+try:
+            a = input("enter: ")
             mon = [
             "January",
             "February",
@@ -32,37 +17,31 @@ def conv(n):
             "December"
             ]
 
-            if "/" in n:
-                    month, day, year = n.split(sep = "/")
+            if "/" in a:
+                    month, day, year = a.split(sep = "/")
                     month = int(month)
                     day = int(day)
                     year = int(year)
-                    '''month = 'month:02'
-                    day = 'day:02'''
+                    if int(month) > 12 or int(day) > 31:
+                        raise ValueError
 
-                    print(f"{year}-{month:02}-{day:02}")
+            elif "," in a:
 
-            elif "," in n:
-                    n = n.replace(",", " ")
-                    day,month,year = n.split()
+                    a = a.replace(",", " ")
+                    day,month,year = a.split()
+                    day = day.strip()
+                    month = month.strip()
+                    year = year.strip()
                     month = mon.index(month)
                     month = int(month)
-                    '''if is_char(month) == True:
-                        month = ("0" + month)
-                        print(month)
-                    if is_char(day) == True:
-                         day = ("0" + day)'''
-                    print(f"{year}-{month:02}-{day:02}")
-            if month > 12 or day > 31:
-                    raise ValueError
-                    pass
+                    if int(month) > 12 or int(day) > 31:
+                            raise ValueError
 
-    except (ValueError,KeyError):
+
+            print(f"{year}-{month:02}-{day:02}")
+
+except (ValueError,KeyError):
+        print("there was an error")
         pass
-def is_char(i):
-    if i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i == "6" or i ==7 or i == 8 or i == 9:
-        return True
-    else:
-        return False
 
-main()
+
