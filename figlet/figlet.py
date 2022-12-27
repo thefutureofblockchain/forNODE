c62139068fdc8566b39ff37c84dbd2df3503cc13
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-27 14:04:03 +0000

    Tue, Dec 27, 2022, 2:04 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -13,6 +13,8 @@ elif len(sys.argv) == 1:
         s = input("Enter: ")
         b = len(figlet.getFonts()) - 1
         a = random.randint(0, b)
+        c = figlet.getFonts()
+        
         figlet.setFont(font = figlet.getFonts(a))
         print(figlet.renderText(s))
 else:
