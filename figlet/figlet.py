bebd7e2f4d93c75a4d085e44672691bb2592d6ed
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-27 13:37:42 +0000

    Tue, Dec 27, 2022, 1:37 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,9 +9,11 @@ req_font = figlet.getFonts()
 print(req_font)
 a = req_font.index(sys.argv[2])
 print(a)
+
 if len(sys.argv) == 2:
         #if sys.argv[1] == "-f" or sys.argv[1] == "--font":
         s = input("Enter: ")
+        s = s.figlet.setfont(sys.argv[2])
         a = figlet.getFonts(sys.argv[2])
         print(a)
         #s = figlet.setFont(font=sys.argv[2])
