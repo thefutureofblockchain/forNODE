fdc8b11bf2562e856e96212af2bbfdaae5f7c43e
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-27 14:00:37 +0000

    Tue, Dec 27, 2022, 2:00 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,5 +1,6 @@
 from pyfiglet import Figlet
 import sys
+import random
 figlet = Figlet()
 if len(sys.argv) == 3:
         if sys.argv[1] == "-f" or sys.argv[1] == "--font":
@@ -8,6 +9,11 @@ if len(sys.argv) == 3:
                 print(figlet.renderText(s))
         else:
                 sys.exit("First arguement wasn't appropriate")
+elif len(sys.argv) == 1:
+        s = input("Enter: ")
+        a = randint(0,len(figlet))
+        figlet.setFont(font = sys.argv[2])
+        print(figlet.renderText(s))
 else:
         sys.exit("Too many or too few arguements")
 
