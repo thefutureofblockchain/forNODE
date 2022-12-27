efabcd99e990f37e3b84cee45e75b359183ba271
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-27 14:12:35 +0000

    Tue, Dec 27, 2022, 2:12 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,15 +2,14 @@ from pyfiglet import Figlet
 import sys
 import random
 figlet = Figlet()
-try:
-        if len(sys.argv) == 3:
+if len(sys.argv) == 3:
                 if sys.argv[1] == "-f" or sys.argv[1] == "--font":
                         s = input("Enter: ")
                         figlet.setFont(font = sys.argv[2])
                         print(figlet.renderText(s))
                 else:
                         sys.exit("First arguement wasn't appropriate")
-        elif len(sys.argv) == 1:
+elif len(sys.argv) == 1:
                 s = input("Enter: ")
                 b = len(figlet.getFonts())
                 a = random.randint(0, b)
@@ -18,10 +17,9 @@ try:
                 d = c[a]
                 figlet.setFont(font = d)
                 print(figlet.renderText(s))
-        else:
+else:
                 sys.exit("Too many or too few arguements")
-except figlet.FontNotFound:
-        sys.exit("not a valid second arg")
+
 
 
 
