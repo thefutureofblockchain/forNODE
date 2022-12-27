fc3bde90efc1c478bf0d845e2b49cd66b4a3a0d6
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-27 13:52:13 +0000

    Tue, Dec 27, 2022, 1:52 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,6 +3,7 @@ import sys
 figlet = Figlet()
 print(len(sys.argv))
 if len(sys.argv) == 3:
+        print("truee")
         if sys.argv[2] == "-f" or sys.argv[2] == "--font":
                 s = input("Enter: ")
                 figlet.setFont(font = sys.argv[1])
