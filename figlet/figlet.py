9b31e4b84ffe2589a27e12e1695f1a3ff0227095
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-27 13:41:04 +0000

    Tue, Dec 27, 2022, 1:41 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,21 +3,17 @@ import sys
 #use sys.argv btwwww
 figlet = Figlet()
 #a  = figlet.getFonts()
-s = input("Enter: ")
+###s = input("Enter: ")
 
-req_font = figlet.getFonts()
-print(req_font)
-a = req_font.index(sys.argv[2])
-print(a)
+#req_font = figlet.getFonts()
+#print(req_font)
+#a = req_font.index(sys.argv[2])
+#print(a)
 
-if len(sys.argv) == 2:
+#if len(sys.argv) == 2:
         #if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-        s = input("Enter: ")
-        s = s.figlet.setfont(sys.argv[2])
-        a = figlet.getFonts(sys.argv[2])
-        print(a)
-        #s = figlet.setFont(font=sys.argv[2])
-        print(s)
-        #print(figlet.renderText(s))
+s = input("Enter: ")
+figlet.setFont(font = sys.argv[2])
+print(figlet.renderText(s))
 
 
