a612c4b98a50716be1d8d58cc7890961dcd4831d
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2022-12-27 14:09:34 +0000

    Tue, Dec 27, 2022, 2:09 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,22 +2,26 @@ from pyfiglet import Figlet
 import sys
 import random
 figlet = Figlet()
-if len(sys.argv) == 3:
-        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+try:
+        if len(sys.argv) == 3:
+                if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+                        s = input("Enter: ")
+                        figlet.setFont(font = sys.argv[2])
+                        print(figlet.renderText(s))
+                else:
+                        sys.exit("First arguement wasn't appropriate")
+        elif len(sys.argv) == 1:
                 s = input("Enter: ")
-                figlet.setFont(font = sys.argv[2])
+                b = len(figlet.getFonts())
+                a = random.randint(0, b)
+                c = figlet.getFonts()
+                d = c[a]
+                figlet.setFont(font = d)
                 print(figlet.renderText(s))
         else:
-                sys.exit("First arguement wasn't appropriate")
-elif len(sys.argv) == 1:
-        s = input("Enter: ")
-        b = len(figlet.getFonts()) 
-        a = random.randint(0, b)
-        c = figlet.getFonts()
-        d = c[a]
-        figlet.setFont(font = d)
-        print(figlet.renderText(s))
-else:
-        sys.exit("Too many or too few arguements")
+                sys.exit("Too many or too few arguements")
+except figlet.FontNotFound:
+        sys.exit("not a valid second arg")
+
 
 
