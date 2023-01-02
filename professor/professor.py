8c86f4ca3ae2fcb4cba98ff69eb264a0388779dc
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-02 13:19:58 +0000

    Mon, Jan 2, 2023, 1:19 PM +00:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,10 +2,18 @@ import random
 
 
 def main():
-    ...
+    lvl = get_level()
 
 def get_level():
-    ...
+    while True:
+        try:
+            a = int(input("Enter: "))
+            if a > 3 and a < 0:
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            continue
 
 
 def generate_integer(level):
