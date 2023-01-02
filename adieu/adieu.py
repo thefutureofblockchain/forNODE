3bdb5c927d3877c49b6ceb3da03cad63706373a1
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-02 11:03:22 +0000

    Mon, Jan 2, 2023, 11:03 AM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,18 +1,12 @@
 import inflect
 p = inflect.engine()
 ab = []
-i = 0
-#mylist = p.join(("apple" , "banana"))
 while True:
     try:
         a = input("")
         ab.append(a)
-        i+=1
     except EOFError:
-        print()
         break
-c = len(ab)
 cd = p.join(ab)
-#for i in range(c):
 print("Adieu, adieu to ", cd)
 
