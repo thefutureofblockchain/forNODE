61433a77250c3da1b1ce13a2c1b4a034d5beac11
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-02 13:36:12 +0000

    Mon, Jan 2, 2023, 1:36 PM +00:00

--- professor/professor.py
+++ professor/professor.py
@@ -3,21 +3,37 @@ import random
 
 def main():
     lvl = get_level()
+    d = generate_integer(lvl)
+    
 
 def get_level():
     while True:
         try:
             a = int(input("Enter: "))
-            if a > 3 and a < 0:
+            if a > 3 or a < 0:
                 raise ValueError
             else:
+                return a
                 break
         except ValueError:
             continue
 
 
 def generate_integer(level):
-    ...
+    try:
+        if level == 1:
+            x = 9
+        elif level == 2:
+            x = 99
+        elif level == 3:
+            x = 999
+        else:
+            raise ValueError
+    except ValueError:
+        pass
+    i = random.randint(0, x )
+    return i
+
 
 
 if __name__ == "__main__":
