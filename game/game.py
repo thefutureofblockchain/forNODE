73aaacf4de89475d2ec8b222e52dd2840bfbe182
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-02 11:21:31 +0000

    Mon, Jan 2, 2023, 11:21 AM +00:00

--- game/game.py
+++ game/game.py
@@ -3,11 +3,26 @@ def main():
     while True:
         try:
             lvl = int(input("Level: "))
-            
+
             if lvl < 0:
                 raise ValueError
             else:
                 break
         except ValueError:
             continue
+    r = random.randint(1, lvl)
+    while True:
+        try:
+            g = int(input("Guess: "))
+            if g == r:
+                print("Just right!")
+            elif g > r:
+                print("Too Large!")
+            elif g < r:
+                print("Too small!")
+            else:
+                raise ValueError
+        except ValueError:
+            continue
+
 main()
