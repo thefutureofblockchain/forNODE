af51a9cc23372d4f6d293861a11cf6a60908a7c2
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-02 15:39:09 +0000

    Mon, Jan 2, 2023, 3:39 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,7 +5,10 @@ try:
     a = sys.argv[1]
     print(float(a))
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    print(json.dumps(response.json(), indent=2))
+
+    o = response.json()
+    for result in o["results"]:
+        print(result["trackName"])
 
 except requests.RequestException:
     pass
