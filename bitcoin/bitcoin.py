ba10dbd525f497798fcb654304f48bb086759d51
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-02 15:49:06 +0000

    Mon, Jan 2, 2023, 3:49 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -7,9 +7,10 @@ try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
     o = response.json()
-    for USD in o["bpi"]:
-        amount = USD["rate_float"]
-        print(f"${amount:,.4f}")
+    print(o, indent = 2 )
+    '''for result in o["bpi"]:
+        amount =
+        print(f"${amount:,.4f}")'''
 
 except requests.RequestException:
     pass
