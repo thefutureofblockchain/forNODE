7e8ac1b082d5ddc10a8ec0775389372eb62e815d
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-03 11:14:34 +0000

    Tue, Jan 3, 2023, 11:14 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -4,10 +4,13 @@ import csv
 
 try:
     with open(sys.argv[1], "r") as file:
+        if len(sys.argv) != 2:
+            raise FileNotFoundError
+        
         reader = csv.reader(file)
         '''read = csv.reader(file)'''
         table = reader
         print(tabulate.tabulate(table, tablefmt="grid"))
 
-except FileNotFoundError:
+except (FileNotFoundError,IndexError):
     sys.exit("there was an error")
