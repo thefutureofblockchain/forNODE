52af2a5c9559c353604db27bde557b404a711cf2
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-03 11:02:20 +0000

    Tue, Jan 3, 2023, 11:02 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -4,6 +4,8 @@ import csv
 
 try:
     with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(csvfile)
+        print(tabulate(table, headers, tablefmt="plain"))
 
 except FileNotFoundError:
     pass
