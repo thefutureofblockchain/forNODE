f2ca7ccc61f7f44f8d40478ff9a47ceb9a7d2ba7
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 05:30:43 +0000

    Wed, Jan 4, 2023, 5:30 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,10 +3,13 @@ import csv
 #try:
 students = []
 with open(sys.argv[1]) as file:
-        reader = csv.Dicteader(file)
+        reader = csv.reader(file)
         for row in reader:
-            students.append({"name": row[0], "house": row[1]})
-            b = students.index(row[0])
-            a = students[b]
-            a = a.rstrip().split(",")
-            print(a)
+            if "name" in row:
+                pass
+            else:
+                students.append({"name": row[0], "house": row[1]})
+                b = students.index(row[0])
+                a = students[b]
+                a = a.rstrip().split(",")
+                print(a)
