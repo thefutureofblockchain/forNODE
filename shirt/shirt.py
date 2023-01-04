e2b839b6c8979c3701822fc30260c58464e7fef8
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 06:44:26 +0000

    Wed, Jan 4, 2023, 6:44 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -3,11 +3,11 @@ import sys
 try:
         sys.argv[1] = sys.argv[1].lower()
         sys.argv[2] = sys.argv[2].lower()
-        if len(sys.argv) != 3 or sys.argv[1].endswith("png") == False or sys.argv[1].endwith("jpeg") == False or sys.argv[1].endswith("jpg") == False or sys.argv[2].endswith("png") == False or sys.argv[2].endwith("jpeg") == False or sys.argv[2].endswith("jpg") == False:
+        if len(sys.argv) != 3 or sys.argv[1].endswith("png") == False or sys.argv[1].endwith("jpeg") == False or sys.argv[1].endswith("jpg") == False or sys.argv[2].endswith("png") == False or sys.argv[2].endwith("jpeg") == False or sys.argv[2].endswith("jpg") == False or os.path.splitext:
             raise ValueError
         else:
             pass
-        
+
 except(FileNotFoundError,IndexError,ValueError):
     sys.exit("uh")
 
