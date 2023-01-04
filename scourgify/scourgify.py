ad6d0b4e6ca013ba7bf37d2de87bc6a994db4530
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 06:07:33 +0000

    Wed, Jan 4, 2023, 6:07 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,29 +3,27 @@ import csv
 #try:
 students = []
 q = []
-with open(sys.argv[1]) as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if "name" in row:
-                pass
-            else:
-                students.append({"name": row[0], "house": row[1]})
-                '''b = students.index(row[0])
-                a = students[b]
-                a = a.rstrip().split(",")
-                print(a)'''
-for student in students:
-#b = students.index()
-    a = student['name']
-    b = student['house']
-    n1,n2 = a.split(",")
-    ab = [n2,n1,b]
-    q.append(ab)
-with open(sys.argv[2], "a") as file:
-    writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
-    writer.writeheader()
-    for line in q:
-            print(line)
-            writer.writerow({"first": line[0],"last": line[1], "house": line[2]})
-
+try:
+    with open(sys.argv[1]) as file:
+            if len(sys.argv) != 2 or sys.argv[2].endswith(".csv") == False:
+                raise ValueError
+            reader = csv.reader(file)
+            for row in reader:
+                if "name" in row:
+                    pass
+                else:
+                    students.append({"name": row[0], "house": row[1]})
+    for student in students:
+        a = student['name']
+        b = student['house']
+        n1,n2 = a.split(",")
+        ab = [n2,n1,b]
+        q.append(ab)
+    with open(sys.argv[2], "a") as file:
+        writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
+        writer.writeheader()
+        for line in q:
+                writer.writerow({"first": line[0],"last": line[1], "house": line[2]})
+except (FileNotFoundError,ValueError,IndexError):
+    sys.exit("uh oh")
 
