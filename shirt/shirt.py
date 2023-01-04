1627b91edaebb67de70cd46ae12291099732d3e0
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 06:55:03 +0000

    Wed, Jan 4, 2023, 6:55 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -2,12 +2,18 @@ from PIL import Image, ImageOps
 import sys
 import os
 try:
+        inp = sys.argv[1]
+        outp = sys.argv[2]
         sys.argv[1] = sys.argv[1].lower()
         sys.argv[2] = sys.argv[2].lower()
         if len(sys.argv) != 3 or sys.argv[1].endswith("png") == False or sys.argv[1].endswith("jpeg") == False or sys.argv[1].endswith("jpg") == False or sys.argv[2].endswith("png") == False or sys.argv[2].endwith("jpeg") == False or sys.argv[2].endswith("jpg") == False or os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
             raise ValueError
         else:
             pass
+        shirt = Image.open("shirt.png")
+        size = shirt.size
+        inp.paste(shirt, shirt)
+
 
 except(FileNotFoundError,IndexError,ValueError):
     sys.exit("uh")
