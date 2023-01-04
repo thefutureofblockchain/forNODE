c0e75d407229df3e5f08e9863a81c30ac8affb5c
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 18:22:06 +0000

    Wed, Jan 4, 2023, 6:22 PM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -7,18 +7,17 @@ def main():
         outp = sys.argv[2]
         sys.argv[1] = sys.argv[1].lower()
         sys.argv[2] = sys.argv[2].lower()
-        '''if len(sys.argv) != 3 or sys.argv[1].endswith("png") == False or sys.argv[1].endswith("jpeg") == False or sys.argv[1].endswith("jpg") == False or sys.argv[2].endswith("png") == False or sys.argv[2].endwith("jpeg") == False or sys.argv[2].endswith("jpg") == False or os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
+        if len(sys.argv) != 3 or sys.argv[1].endswith("png","jpeg","jpg") == False or sys.argv[2].endswith("png","jpeg","Jpg") == False or os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
             raise ValueError
         else:
-            pass'''
+            pass
         shirt = Image.open("shirt.png")
         inp1 = Image.open(inp)
         size = shirt.size
         size2 = inp1.size
 
-        inp1 = ImageOps.fit(shirt,size2, method=Image.Resampling.BICUBIC, bleed=0.0, centering=(0.5, 0.5))
-        inp1.paste(shirt, shirt)
-        print(size2, size)
+        updatedshirt = ImageOps.fit(shirt,size2, method=Image.Resampling.BICUBIC, bleed=0.0, centering=(0.5, 0.5))
+        inp1.paste(updatedshirt, updatedshirt)
         inp1.save(outp)
 main()
 
