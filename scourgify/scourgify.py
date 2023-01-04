cc32ac9e5d4b5647df9f2cabc78016144f318673
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 05:18:54 +0000

    Wed, Jan 4, 2023, 5:18 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,1 +1,8 @@
-wget https://cs50.harvard.edu/python/2022/psets/6/scourgify/before.csv
+import sys
+try:
+    with open(sys.argv[1]) as file:
+        for line in file:
+            name, house = line.rstrip().split(",")
+            print(f"{name} is in {house}")
+except (FileNotFoundError, ValueError, IndexError):
+    sys.exit("uh oh")
