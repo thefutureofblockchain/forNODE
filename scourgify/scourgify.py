8f40982d8941dcd63b906c126b53fe664257bb76
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 05:35:53 +0000

    Wed, Jan 4, 2023, 5:35 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -9,7 +9,13 @@ with open(sys.argv[1]) as file:
                 pass
             else:
                 students.append({"name": row[0], "house": row[1]})
-                b = students.index(row[0])
+                '''b = students.index(row[0])
                 a = students[b]
                 a = a.rstrip().split(",")
-                print(a)
+                print(a)'''
+for student in students:
+#b = students.index()
+    a = students[0]
+    #a = a.rstrip().split(",")
+    print(a)
+    #print(f"{student['name']} is from {student['house']}")
