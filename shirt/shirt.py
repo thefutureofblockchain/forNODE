c001e8bc1afc12af01e5527722b7a3ed557d7a52
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 06:39:33 +0000

    Wed, Jan 4, 2023, 6:39 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,2 +1,18 @@
-import Pillow
-def main():
+from PIL import Image, ImageOps
+import sys
+try:
+    def main():
+        if is_valid(sys.argv) == False:
+            raise ValueError
+        else:
+            pass
+        
+    def is_valid(n):
+        n[1] = n[1].lower()
+        n[2] = n[2].lower()
+        if len(n) != 3 or n[1].endswith("png") == False or n[1].endwith("jpeg") == False or n[1].endswith("jpg")or n[2].endswith("png") == False or n[2].endwith("jpeg") == False or n[2].endswith("jpg") == False:
+            return False
+        else:
+            return True
+except(FileNotFoundError,IndexError,ValueError):
+    sys.exit("uh")
