bb3f4057ba9bcfa8c3b52582a7f5befd33b4b4ea
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-04 06:58:11 +0000

    Wed, Jan 4, 2023, 6:58 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -6,13 +6,15 @@ try:
         outp = sys.argv[2]
         sys.argv[1] = sys.argv[1].lower()
         sys.argv[2] = sys.argv[2].lower()
-        if len(sys.argv) != 3 or sys.argv[1].endswith("png") == False or sys.argv[1].endswith("jpeg") == False or sys.argv[1].endswith("jpg") == False or sys.argv[2].endswith("png") == False or sys.argv[2].endwith("jpeg") == False or sys.argv[2].endswith("jpg") == False or os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
+        '''if len(sys.argv) != 3 or sys.argv[1].endswith("png") == False or sys.argv[1].endswith("jpeg") == False or sys.argv[1].endswith("jpg") == False or sys.argv[2].endswith("png") == False or sys.argv[2].endwith("jpeg") == False or sys.argv[2].endswith("jpg") == False or os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
             raise ValueError
         else:
-            pass
+            pass'''
         shirt = Image.open("shirt.png")
+        inp1 = Image.open(inp)
         size = shirt.size
-        inp.paste(shirt, shirt)
+        inp1.paste(shirt, shirt)
+        
 
 
 except(FileNotFoundError,IndexError,ValueError):
