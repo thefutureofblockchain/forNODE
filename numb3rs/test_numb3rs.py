7bc1552159be27b91f77d0c8467855c9dfede061
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-05 11:47:18 +0000

    Thu, Jan 5, 2023, 11:47 AM +00:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,7 +1,7 @@
 from numb3rs import validate
 def test_range():
     assert validate("265.3.3.2") == False
-    assert validate("255.3.3.2") == True
+    assert validate("252.3.3.2") == True
 def test_validity():
     assert validate("@22.ss.abc.s") == False
     assert validate("12.2.2") == False
