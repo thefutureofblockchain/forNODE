1cc58e529653c482bc80479ea2bc06c0c8ab9b4d
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-06 09:13:07 +0000

    Fri, Jan 6, 2023, 9:13 AM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -4,7 +4,7 @@ def main():
     print(parse(input("HTML: ")))
 def parse(s):
 
-    string = re.search(r"<iframe width="560" height="315" src="(https://www.youtube.com/embed/xvFZjo5PgG0)" title="YouTube video player" frameborder="0" allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen\>\<\iframe\>","",s)
+    string = re.search(r'src="(https://www.youtube.com/embed/xvFZjo5PgG0)" ',s)
     if string:
         print(string)
     else:
