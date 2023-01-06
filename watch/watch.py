6318eb64771515496b28dbf6193c4a3287fe5a4c
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-06 09:57:42 +0000

    Fri, Jan 6, 2023, 9:57 AM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -6,9 +6,9 @@ def parse(s):
 
     string = re.search(r'src="(http(?:s)?://(?:www.)youtube.com/embed/[\w]+)',s)
     if string:
-        print(string)
+        a = string.groups(1).replace("youtube","you)
     else:
         print(string)
-        print("SEE BITCH")
+        print("SEE")
 if __name__ == "__main__":
     main()
