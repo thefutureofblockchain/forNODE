c09c4d74ae109e1fb0752ffa073f8d2f348602b2
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-06 09:10:14 +0000

    Fri, Jan 6, 2023, 9:10 AM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -3,7 +3,11 @@ import sys
 def main():
     print(parse(input("HTML: ")))
 def parse(s):
-    
-    string = re.sub("","",s)
+
+    string = re.search(r"<iframe width="560" height="315" src="(https://www.youtube.com/embed/xvFZjo5PgG0)" title="YouTube video player" frameborder="0" allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen\>\<\iframe\>","",s)
+    if string:
+        print(string)
+    else:
+        print("SEE BITCH")
 if __name__ == "__main__":
     main()
