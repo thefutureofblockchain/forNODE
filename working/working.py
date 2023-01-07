b16704ab0df2fcdf812e9b6c9f05d59afaf458bf
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-07 14:06:08 +0000

    Sat, Jan 7, 2023, 2:06 PM +00:00

--- working/working.py
+++ working/working.py
@@ -8,10 +8,10 @@ def main():
 
 def convert(s):
     try:
-        a = re.search(r"((?:^[0-9]|1[1-2])\:[0-5][0-9] [AM|PM] to (?:[0-9]|1[1-2])\:[0-5][0-9] [AM|PM]$)",s)
-        b = re.search(r"",s)
-        if a:
-            print(a)
+        a = re.search(r"((?:^[0-9]|1[1-2])\:[0-5][0-9] (?:AM|PM) to (?:[0-9]|1[1-2])\:[0-5][0-9] (?:AM|PM)$)",s)
+        b = re.search(r"((?:^[0-9]|1[1-2]) (?:AM|PM) to (?:[0-9]|1[1-2]) (?:AM|PM)$)",s)
+        if a or b:
+            print(a,b)
         else:
             print("hello")
     except ValueError:
