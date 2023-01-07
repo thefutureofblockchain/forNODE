ea8714bcd063f3072b7eab73fe4eac35a85ee8dc
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-07 13:49:13 +0000

    Sat, Jan 7, 2023, 1:49 PM +00:00

--- working/working.py
+++ working/working.py
@@ -8,9 +8,11 @@ def main():
 
 def convert(s):
     try:
-        a = re.search(r"[\d]")
+        a = re.search(r"([0-9]|1[1-2])\:[0-5][0-9]")
+        if a:
+            print(a)
     except ValueError:
-
+        
 
 ...
 
