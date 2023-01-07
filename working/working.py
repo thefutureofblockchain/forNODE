8143fd30ba2d4f1e0083aa0769a69d056b325fe5
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-07 14:12:48 +0000

    Sat, Jan 7, 2023, 2:12 PM +00:00

--- working/working.py
+++ working/working.py
@@ -10,12 +10,20 @@ def convert(s):
     try:
         a = re.search(r"((?:^[0-9]|1[1-2])\:[0-5][0-9] (?:AM|PM) to (?:[0-9]|1[1-2])\:[0-5][0-9] (?:AM|PM)$)",s)
         b = re.search(r"((?:^[0-9]|1[1-2]) (?:AM|PM) to (?:[0-9]|1[1-2]) (?:AM|PM)$)",s)
-        if a or b:
-            return 
+        if a:
+            q = re.search()
+            ab = a.groups(1).replace(q.groups(1),)
+            return a
+        elif b:
+            ab = str(b.groups(1))
+            q = re.search(r"(?:[0-9]|1[1-2]) PM", ab)
+            if q:
+                print(q.groups(1))
+            #ab = b.groups(1).replace(q.groups(1),)
         else:
-            print("hello")
+            raise ValueError
     except ValueError:
-        pass
+        sys.exit("did not work bro")
 
 ...
 
