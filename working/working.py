18c3324008f1e0fb294f0307a21798bde3122050
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-07 14:47:10 +0000

    Sat, Jan 7, 2023, 2:47 PM +00:00

--- working/working.py
+++ working/working.py
@@ -18,31 +18,13 @@ def convert(s):
             ab = str(b.groups(1))
             q = re.search(r"([0-9]|1[1-2]) PM", ab)
             if q:
-                print(q)
-                if q.groups() == 1:
-                    qa = str(q.groups(1))
-                    qa = qa.replace("('","")
-                    qa = qa.replace("',)","")
-                    print(qa)
-                    qa = int(qa)
-                    qa = qa+12
-                    print(qa)
-                if q.groups() == 2:
-                    qa = str(q.groups(1))
-                    qa = qa.replace("('","")
-                    qa = qa.replace("',)","")
-                    print(qa)
-                    qa = int(qa)
-                    qa = qa+12
-                    print(qa)
-                    qb = str(q.groups(2))
-                    qb = qb.replace("('","")
-                    qb = qb.replace("',)","")
-                    print(qb)
-                    qb = int(qb)
-                    qb = qb+12
-                    print(qb)
-                    print(qa ,":" ,qb)
+                qa = str(q.groups(1))
+                qa = qa.replace("('","")
+                qa = qa.replace("',)","")
+                print(qa)
+                qa = int(qa)
+                qa = qa+12
+                print(qa)
             #ab = b.groups(1).replace(q.groups(1),)
         else:
             raise ValueError
