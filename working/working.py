12599d48cc7abd697e15a955545753eb163c5376
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-07 16:11:25 +0000

    Sat, Jan 7, 2023, 4:11 PM +00:00

--- working/working.py
+++ working/working.py
@@ -19,16 +19,17 @@ def convert(s):
             q = re.search(r"([0-9]|1[1-2]) PM", ab)
             if q:
                 abc = str(q.groups)
-                q,a = re.split("to",abc)
-                print(q,a)
-                print(len((q.groups(0))))
+                print(abc)
+                #q,a = re.split("to",abc)
+                #print(q,a)
+                '''print(len((q.groups(0))))
                 qa = str(q.groups(1))
                 qa = qa.replace("('","")
                 qa = qa.replace("',)","")
                 print(qa)
                 qa = int(qa)
                 qa = qa+12
-                print(qa)
+                print(qa)'''
             #ab = b.groups(1).replace(q.groups(1),)
         else:
             raise ValueError
