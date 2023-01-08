67769f14b1308bceedda3ccf67ed5dcfd7ff3867
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-08 14:09:27 +0000

    Sun, Jan 8, 2023, 2:09 PM +00:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,9 +1,9 @@
 from working import convert
 import pytest
 def test_1():
-    with pytest.raises(ValueError):
-        convert("cat")
-    #assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    #with pytest.raises(ValueError):
+        #convert("cat")
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
 def test_2():
     pass
 def test_3():
