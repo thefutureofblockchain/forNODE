6c746e166463ad026eda35c4ecb6e974c1f167cc
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-08 14:05:40 +0000

    Sun, Jan 8, 2023, 2:05 PM +00:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,7 +1,8 @@
 from working import convert
 def test_1():
-    with pytest.raises(ValueError)
-    assert convert("cat")
+    with pytest.raises(ValueError):
+        convert("cat")
+    
 def test_2():
     pass
 def test_3():
