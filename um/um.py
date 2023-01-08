c144a7c278f9abe533aca04b84ba1054c8b94526
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-08 17:38:57 +0000

    Sun, Jan 8, 2023, 5:38 PM +00:00

---
+++ um/um.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    b = 0
+    s = s.split(" ")
+    for word in s:
+        q = re.search("[!@#$%^&*() ]um[!@#$%^&*() ]",word)
+        if q:
+            b+=1
+    return b
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
