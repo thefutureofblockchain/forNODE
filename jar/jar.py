790160b091da4cdc32d9dd653399f125a66dc6f9
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-09 11:59:20 +0000

    Mon, Jan 9, 2023, 11:59 AM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,6 +1,6 @@
 class Jar:
     def __init__(self, capacity=12):
-        if capacity >= 0:
+        if capacity <= 0:
             raise ValueError
         self.capacity = capacity
         self.contain = 0
