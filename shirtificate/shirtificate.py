5b9dd2936c83fc33f69ff8b1c8caccefb5ac201f
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-09 16:53:43 +0000

    Mon, Jan 9, 2023, 4:53 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -8,9 +8,13 @@ class PDF(FPDF):
         self.cell(30, 10, "CS50 SHIRTIFICATE", border=1, align="C")
         # Performing a line break:
         self.ln(20)
+name = input(": ")
+name = f"{name} took CS50"
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
 pdf.cell(0, 0, 'CS50 SHIRTIFICATE', align='C')
 pdf.image("shirtificate.png", x=0, y = 50)
+pdf.set_text_color(0)
+pdf.cell(-23,112,name,align="C")
 pdf.output("new-tuto2.pdf")
