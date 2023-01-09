52ddab5dc0006014ed9b2d506fbffe28900930a4
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-09 11:24:08 +0000

    Mon, Jan 9, 2023, 11:24 AM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,3 +1,3 @@
 from seasons import get_difference, conv
 def test_getdifference():
-    assert get_difference("2009-12-1") == 689320
+    assert conv(689320) == "Six million, eight hundred ninety-three thousand, two hundred eighty minutes"
