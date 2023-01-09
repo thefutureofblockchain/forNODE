4f8aef861357917f25dcac8dadbda2bfdcffcd54
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-09 15:58:45 +0000

    Mon, Jan 9, 2023, 3:58 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,5 +2,5 @@ from fpdf import FPDF
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
-pdf.cell(60,10,"CS50 SHIRTIFICATE", align="C")
+pdf.cell(60, 10, 'Powered by FPDF.', new_x="LMARGIN", new_y="NEXT", align='C')
 pdf.output("new-tuto2.pdf")
