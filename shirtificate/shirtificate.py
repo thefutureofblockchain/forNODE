2b7bdf624c1837ecdd03c56fddc36cc9ecd752dc
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-09 17:01:55 +0000

    Mon, Jan 9, 2023, 5:01 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,19 +1,15 @@
 from fpdf import FPDF
-class PDF(FPDF):
+class pdf(FPDF):
     def header(self):
         self.set_font("helvetica", "B", 15)
-        # Moving cursor to the right:
-        self.cell(80)
-        # Printing title:
-        self.cell(30, 10, "CS50 SHIRTIFICATE", border=1, align="C")
-        # Performing a line break:
-        self.ln(20)
+        self.set_text_color(0)
+        self.cell(0, 0, "CS50 SHIRTIFICATE", align="C")
 name = input(": ")
 name = f"{name} took CS50"
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
-pdf.cell(0, 0, 'CS50 SHIRTIFICATE', align='C')
+#pdf.cell(0, 0, 'CS50 SHIRTIFICATE', align='C')
 pdf.image("shirtificate.png", x=0, y = 50)
 pdf.set_text_color(0)
 pdf.cell(-23,112,name,align="C")
