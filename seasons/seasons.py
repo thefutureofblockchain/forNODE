348d5511e6805ab57be19c1d6349989cbcefb940
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-09 09:31:58 +0000

    Mon, Jan 9, 2023, 9:31 AM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,11 +1,16 @@
 from datetime import date
 import inflect
+import sys
 
 def main():
-    dateInputted = input("year: ")
-    dateInputted = dateInputted.split("-")
-    print(date())
-    print(date.today())
+    try:
+        dateInputted = input("year: ")
+        year, month, _date = dateInputted.split("-")
+        
+        print(date(year,month,_date))
+        print(date.today())
+    except ValueError:
+        sys.exit("Something went wrong")
 
 
 ...
