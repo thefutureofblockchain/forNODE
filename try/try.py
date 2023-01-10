7e329bebd6ad990bb61b10a4523bdb71b0272cdd
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 11:22:35 +0000

    Tue, Jan 10, 2023, 11:22 AM +00:00

--- try/try.py
+++ try/try.py
@@ -1,10 +1,28 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer
+import random
 
 
 class EscapeRoomApp(App):
     """A Textual for a terminal based game."""
+    COLORS = [
+        "white",
+        "maroon",
+        "red",
+        "purple",
+        "fuchsia",
+        "olive",
+        "yellow",
+        "navy",
+        "teal",
+        "aqua",
+    ]
 
+    def on_mount(self) -> None:
+        colors = self.COLORS
+        for _ in range(5):
+            self.screen.styles.background = random.choice(colors)
+            self.screen.styles.background = random.choice(colors)
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
