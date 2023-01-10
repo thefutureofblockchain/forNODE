9fabcf16273866d1edd45abce89428b84057b288
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 10:18:08 +0000

    Tue, Jan 10, 2023, 10:18 AM +00:00

--- project/project.py
+++ project/project.py
@@ -3,7 +3,7 @@ import curses
 def main(stdscr):
     # Clear screen
     stdscr.clear()
-
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
     stdscr.addstr("Pretty text", curses.color_pair(1))
     stdscr.refresh()
     stdscr.getkey()
