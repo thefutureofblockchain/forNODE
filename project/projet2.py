f07f0e5ad711d4d14f7364bd070cd2145cbecb60
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 12:19:17 +0000

    Tue, Jan 10, 2023, 12:19 PM +00:00

--- project/projet2.py
+++ project/projet2.py
@@ -1,8 +1,12 @@
 import rich
 import os
 import time
+import curses
 os.system("ls")
 time.sleep(2)
 os.system('clear')
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
 
 rich.print("[red]hello")
