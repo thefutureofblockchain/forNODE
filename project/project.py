5a6a2a51105821eaf5ca2e210d1d0a1bc32cc8fb
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 10:58:24 +0000

    Tue, Jan 10, 2023, 10:58 AM +00:00

--- project/project.py
+++ project/project.py
@@ -3,13 +3,12 @@ from rich import print as rprint
 def main(stdscr):
     # Clear screen
     stdscr.clear()
+    d = 0
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     stdscr.addstr("Pretty text", curses.color_pair(1))
-    while True:
-        c = stdscr.getkey()
-        if c == "k":
+    c = stdscr.getkey()
+    if c == "k":
             q = str(c)
-            break
     stdscr.addstr(q)
     stdscr.refresh()
     stdscr.addstr(c)
