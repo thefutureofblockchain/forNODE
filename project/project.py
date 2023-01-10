5e3ce3869b585e923177e0f0e371392a17cb8e31
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 10:56:28 +0000

    Tue, Jan 10, 2023, 10:56 AM +00:00

--- project/project.py
+++ project/project.py
@@ -5,7 +5,12 @@ def main(stdscr):
     stdscr.clear()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     stdscr.addstr("Pretty text", curses.color_pair(1))
-    c = stdscr.getkey()
+    while True:
+        c = stdscr.getkey()
+        if c == "k":
+            q = str(c)
+            break
+    stdscr.addstr(q)
     stdscr.refresh()
     stdscr.addstr(c)
     curses.echo()
