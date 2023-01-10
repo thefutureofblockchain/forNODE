53973337805becf867a7a5e4f2833e4bcf28c1d6
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 10:19:06 +0000

    Tue, Jan 10, 2023, 10:19 AM +00:00

--- project/project.py
+++ project/project.py
@@ -3,6 +3,8 @@ import curses
 def main(stdscr):
     # Clear screen
     stdscr.clear()
+    stdscr.addstr(0, 0, "Current mode: Typing mode",
+              curses.A_BLINK)
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
     stdscr.addstr("Pretty text", curses.color_pair(1))
     stdscr.refresh()
