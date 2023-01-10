bd4c08c9b4a21efc41f8f109fd1f585e13ace9d0
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 10:20:21 +0000

    Tue, Jan 10, 2023, 10:20 AM +00:00

--- project/project.py
+++ project/project.py
@@ -3,10 +3,9 @@ import curses
 def main(stdscr):
     # Clear screen
     stdscr.clear()
-    stdscr.addstr(0, 0, "Current mode: Typing mode",
-              curses.A_BLINK)
+    stdscr.addstr(0, 0, "Current mode: Typing mode",curses.A_BLINK)
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-    stdscr.addstr("Pretty text", curses.color_pair(1))
+    stdscr.addstr("Pretty text", curses.color_pair(0))
     stdscr.refresh()
     stdscr.getkey()
 
