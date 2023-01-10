a52595470f573dbf2a562fb65b088598dfa2518c
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 14:51:57 +0000

    Tue, Jan 10, 2023, 2:51 PM +00:00

--- ancd.py
+++ ancd.py
@@ -4,5 +4,5 @@ my_data = [1, 2, 3]
 my_labels = 'label1', 'label2', 'label3'
 plt.pie(my_data, labels=my_labels, autopct='%1.1f%%')
 plt.title('My Title')
-plt.axis('equal')
+#plt.axis('equal')
 plt.show()
