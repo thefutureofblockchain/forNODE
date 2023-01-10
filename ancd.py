e1c9a6eb9cdcb66fea415b80780a16eab126e2d4
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-10 14:43:39 +0000

    Tue, Jan 10, 2023, 2:43 PM +00:00

--- ancd.py
+++ ancd.py
@@ -1,6 +1,11 @@
 import plotly.express as px
 # This dataframe has 244 lines, but 4 distinct values for `day`
 df = px.data.tips()
+d = []
+cc = {"format" : "British Parli" ,"perc" : 20}
+bc = {"format" : "LD" , "perc" : 20}
+print(cc,bc)
+d.append(cc)
 
-fig = px.pie(df, values='tip', names='day')
+fig = px.pie(df, values='cc["perc"]', names='cc["format"]')
 fig.show()
