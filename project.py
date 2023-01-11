e61fc9eb02dbe81ae3b6e820850cafe9c60f3290
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-11 17:36:59 +0000

    Wed, Jan 11, 2023, 5:36 PM +00:00

--- project.py
+++ project.py
@@ -2,9 +2,12 @@ import rich
 import os
 import time
 import sys
+bp = 0
+pf = 0
+mun = 0
+wsd = 0
 def main():
     os.system("ls")
-    time.sleep(2)
     os.system('clear')
     rich.print("[red]Hello!")
     rich.print("[green]Are you ready?")
@@ -12,18 +15,33 @@ def main():
     if a != "y" and a.lower() != "n":
         sys.exit("Input was incorrect")
     os.system('clear')
-    ques1():
-    ques2():
-    ques3():
-    ques4():
-    ques5():
-    ques6():
-    ques7():
-    ques8():
-    ques9():
-    ques10():
+    ques1()
+    os.system('clear')
+    ques2()
+    os.system('clear')
+    ques3()
+    os.system('clear')
+    ques4()
+    os.system('clear')
+    ques5()
+    os.system('clear')
+    ques6()
+    os.system('clear')
+    ques7()
+    os.system('clear')
+    ques8()
+    os.system('clear')
+    ques9()
+    os.system('clear')
+    ques10()
+    os.system('clear')
+    rich.print(" bp is",  bp ,", pf is",pf,"wsd is",wsd ," and lastly, mun is" ,mun)
 def ques1():
-    rich.print("Are you the kind of person who likes to be anxious?")
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("[dark_green]Are you the kind of person who likes to be anxious?")
     while True:
         Ques1 = input(": ")
         a = Ques1.lower()
@@ -31,6 +49,185 @@ def ques1():
             rich.print("You did not enter a character that was y or n")
         else:
             break
+    if a == "y":
+        bp+=1
+    else:
+        pf+=1
+        mun+=1
+        wsd+=1
+def ques2():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Do you like to lie?")
+    while True:
+        Ques2 = input(": ")
+        a = Ques2.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        bp+=1
+        wsd+=1
+        mun+=1
+    else:
+        pf+=1
+def ques3():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Facts(y) or logic(n)?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        pf+=1
+        mun+=1
+    else:
+        bp+=1
+        wsd+=1
+
+
+def ques4():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Do you have weird, what some might even call oddly specific knowledge?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        pf+=1
+        mun+=1
+    else:
+        bp+=1
+        wsd+=1
+def ques5():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Do you think the speed and quality of the speaker can make or break a debate?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        pf+=1
+    else:
+        bp+=1
+        wsd+=1
+        mun+=1
+def ques6():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Do you think the best things in life are short?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        pf+=1
+        mun+=1
+    else:
+        bp+=1
+        wsd+=1
+def ques7():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("As a child, did you engage in pretend play much?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        mun+=1
+    else:
+        bp+=1
+        wsd+=1
+        pf+=1
+def ques8():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Are you okay with being left out?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        wsd+=1
+    else:
+        bp+=1
+        mun+=1
+        pf+=1
+def ques9():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Truthfully, do you think you are the best in your friend group?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        bp+=1
+    else:
+        pf+=1
+        mun+=1
+        wsd+=1
+def ques10():
+    global bp
+    global pf
+    global mun
+    global wsd
+    rich.print("Do you like to go into a situation fully prepared?")
+    while True:
+        Ques1 = input(": ")
+        a = Ques1.lower()
+        if a != "y" and a != "n":
+            rich.print("You did not enter a character that was y or n")
+        else:
+            break
+    if a == "y":
+        pf+=1
+        mun+=1
+    else:
+        wsd+=1
+        bp+=1
 
 if __name__ == "__main__":
     main()
