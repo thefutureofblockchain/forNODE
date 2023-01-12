b621c336a886b5dbbc9749edc27ececb2cd85c6a
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-12 15:44:31 +0000

    Thu, Jan 12, 2023, 3:44 PM +00:00

--- project/test_project.py
+++ project/test_project.py
@@ -2,7 +2,7 @@ import pytest
 from project import ques1,more
 def test_more():
     assert more(10,1,1,1) == 10
-def test_ques10():
-    pass
+def test_ques1():
+    assert ques1() == 1
 def test_ques2():
     pass
