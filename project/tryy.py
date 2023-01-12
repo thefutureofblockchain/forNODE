7a9db3dbac4b7428df3f70dc184bc07bb99933fc
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-12 14:09:18 +0000

    Thu, Jan 12, 2023, 2:09 PM +00:00

--- project/tryy.py
+++ project/tryy.py
@@ -6,6 +6,7 @@ def pdf_ize(format= "a", description="b"):
     pdf.cell(0, 0, "Your Analysis:", align="C")
     pdf.image("debate_analysis.png", x=-75, y=60)
     c = "hello world"
-    pdf.cell(0,-50, c)
-    pdf.cell(0,-50, c)
+    pdf.cell(99,50, c)
+    pdf.cell(-99,100, c)
     pdf.output("pdf-with-image.pdf")
+pdf_ize()
