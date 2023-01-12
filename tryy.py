63f39da57f746507a1cfd02d93db5dda1ff724fa
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-12 06:56:34 +0000

    Thu, Jan 12, 2023, 6:56 AM +00:00

--- tryy.py
+++ tryy.py
@@ -13,9 +13,9 @@ def animator(filenames,delay = 1, repeat = 10):
     for name in filenames:
         with open (name,"r",encoding="utf8") as f:
 		    frames.append(f.readlines())
-	    for i in range(repeat):
-	        for frame in frames:
-		        print("".join(frame))
-		time.sleep(delay)
-		os.system('clear')
+	for i in range(repeat):
+	    for frame in frames:
+		    print("".join(frame))
+	time.sleep(delay)
+	os.system('clear')
 animator(filenames,delay = 0.1,  repeat = 5)
