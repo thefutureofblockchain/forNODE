7f4e2b56085cfa4e5f113f75bf9f20b34374da41
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-12 06:25:59 +0000

    Thu, Jan 12, 2023, 6:25 AM +00:00

--- extra.py
+++ extra.py
@@ -1,4 +1,6 @@
-import time
-while time.sleep == 0.1:
-  tput cup $((RANDOM%LINES)) $((RANDOM%COLUMNS))
-  print("*")
+import sys
+
+def status(percent):
+    sys.stdout.write("%3d%%\r" % percent)
+    sys.stdout.flush()
+status(4)
