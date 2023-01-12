5091004bedb7b4c32c22c782afef56923ccf4c46
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-12 14:15:51 +0000

    Thu, Jan 12, 2023, 2:15 PM +00:00

--- project/tryy.py
+++ project/tryy.py
@@ -5,8 +5,8 @@ def pdf_ize(format= "a", description="b"):
     pdf.set_font("helvetica", "B", 30)
     pdf.cell(0, 0, "Your Analysis:", align="C")
     pdf.image("debate_analysis.png", x=-75, y=60)
+    pdf.cell(-375, 550, description)
     c = "hello world"
-    pdf.cell(99,50, c)
     pdf.cell(-375,550,format,align="C")
     pdf.output("pdf-with-image.pdf")
 pdf_ize()
