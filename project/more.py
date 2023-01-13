42f1412c5a634e039f3f0485d70119fd275697b9
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-01-13 06:10:22 +0000

    Fri, Jan 13, 2023, 6:10 AM +00:00

--- project/more.py
+++
@@ -1,9 +0,0 @@
-def more (a,b,c,d):
-    if a>b and a>c and a>d:
-        return a
-    elif b>a and b>c and b>d:
-        return b
-    elif c>a and c>b and c>d:
-        return c
-    else:
-        return d
