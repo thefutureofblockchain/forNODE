f34c3be4469e2a5c3c3c0139e58b7fcfe20f2623
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-02-01 17:08:31 +0000

    Wed, Feb 1, 2023, 5:08 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(void)
+//wenclair tho 
 {
     string n  = get_string("Input your name: ");
-    
+    // to do : things
     printf("hello, %s\n", n);
 }
