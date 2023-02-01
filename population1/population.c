acca387b56332e868b9f3c7611b2aa7f97b9dc13
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-02-01 16:24:55 +0000

    Wed, Feb 1, 2023, 4:24 PM +00:00

--- population1/population.c
+++ population1/population.c
@@ -1,19 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void get_start_size(int n);
-void get_end_size(int n);
+int get_start_size(void);
+int get_end_size(void);
 int return_years(int n, int m);
 
 int main(void)
 {
         int n = get_start_size();
-        printf("a%i", n )
+        printf("a%i", n );
         int sizee = get_end_size();
     // TODO: Prompt for end size
     // TODO: Calculate number of years until we reach threshold
     // TODO: Print number of years
-}
-void get_start_size (int n) {
+int get_start_size (void) {
         int n = get_int("Starting Size: ");
 }
