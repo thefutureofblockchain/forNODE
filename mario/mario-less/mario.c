efa550da9b4981d96c0bf51f607ed88ec9fe6835
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-02-01 17:35:18 +0000

    Wed, Feb 1, 2023, 5:35 PM +00:00

--- mario/mario-less/mario.c
+++ mario/mario-less/mario.c
@@ -1,17 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_number(void);
+
 int main(void)
 {
-    int x = 10;
+    int x = get_number();
     int y = 1;
     for (int i = 0; i < x; i++) {
         for (int q = x-i-1; q > 0; q--) {
                 printf(".");
             }
-        for (int j = x-i; j < x; j++) {
+        for (int j = x-i-1; j < x; j++) {
             printf("#");
         }
     printf("\n");
     }
+}
+int get_number(void)
+{
+    int a;
+    do
+    {
+    a = get_int("enter: ");
+    }
+    while (a < 0 || a > 8);
+    return a;
 }
