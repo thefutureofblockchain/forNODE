0421abe5d18f923cb214f3dac871d4bf98c5e01c
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-02-01 17:21:47 +0000

    Wed, Feb 1, 2023, 5:21 PM +00:00

--- mario/mario-less/mario.c
+++ mario/mario-less/mario.c
@@ -7,7 +7,7 @@ int main(void)
     int y = 1;
     for (int i = 0; i < x; i++) {
         for (int j = x-i; j < x; j++) {
-            printf("sheba");
+            printf("#");
         }
     printf("\n");
     }
