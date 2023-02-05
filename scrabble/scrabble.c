d5ea087b49ed59fe55f31c653f930f5f4c69aaf8
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-02-05 14:57:02 +0000

    Sun, Feb 5, 2023, 2:57 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -29,7 +29,7 @@ int compute_score(string word)
     printf("%i\n", a);
     for (int i = 0; i<=a; i++) {
       printf("%c\n", word[i]);
-      printf("%i\n",POINTS[i]);
+      printf("%i%i\n",POINTS[i], i);
     }
-    return POINTS[2];
+    return POINTS[0];
 }
