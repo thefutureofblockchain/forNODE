64da1830155df435f83c8a7b9b57c16d6958d2e2
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-03-30 15:49:07 +0000

    Thu, Mar 30, 2023, 3:49 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int isnumber(int c, int a);
+int isnumber(int c, string a);
 
-int main(int argc,  argv)
+int main(int argc,  string argv[])
 {
     if (isnumber(argc, argv[1]) == 1) {
         return 1;
@@ -15,8 +15,9 @@ int main(int argc,  argv)
 
 // in is number implement it in a way so as to ensure it is a number as well as the side effect of print
 //additionally, you need to have something that checks whether it is a number or not.
-int isnumber(int c, int a) {
-    if (isdigit(a)) {
+int isnumber(int c, string a) {
+    
+    if (isalpha(a)) {
         return 1;
         printf("Error: Usage is wrong.");
     }
