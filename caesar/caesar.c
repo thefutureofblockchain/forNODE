fa05b1aa4e4670fbf02048c1ce80c9ffee18b6c9
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-03-30 17:26:40 +0000

    Thu, Mar 30, 2023, 5:26 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,26 +2,13 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int isnumber(int c, string a);
 
 int main(int argc,  string argv[])
 {
-    if (isnumber(argc, argv[1]) == 1) {
+    if (argc != 2) {
+        printf("error\n");
         return 1;
     }
     string k = argv[1];
 
-}
-
-// in is number implement it in a way so as to ensure it is a number as well as the side effect of print
-//additionally, you need to have something that checks whether it is a number or not.
-int isnumber(int c, string a) {
-
-    if (c !=  2 ) {
-        return 1;
-        printf("error");
-    }
-    else {
-        return 0;
-    }
 }
