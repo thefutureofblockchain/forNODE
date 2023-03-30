75739a838d715840420d20d0aa94818b0f762ec7
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-03-30 17:04:39 +0000

    Thu, Mar 30, 2023, 5:04 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -16,12 +16,8 @@ int main(int argc,  string argv[])
 // in is number implement it in a way so as to ensure it is a number as well as the side effect of print
 //additionally, you need to have something that checks whether it is a number or not.
 int isnumber(int c, string a) {
-    
-    if (isalpha(a)) {
-        return 1;
-        printf("Error: Usage is wrong.");
-    }
-    else {
-        return 0;
+
+    if (strlen(a) >  2 ) {
+
     }
 }
