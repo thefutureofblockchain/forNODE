4ce1d257a014e8d2694e87dce7703b22ff0176a5
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-03-30 15:36:51 +0000

    Thu, Mar 30, 2023, 3:36 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int isnumber(int argc, string a);
+
 int main(int argc, string argv[])
 {
-    
+    if (isnumber(argc, argb[1]) == 1) {
+        return 1;
+    }
+    string k = argv[1]
+
 }
