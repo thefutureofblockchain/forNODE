93ad7b2d33fb21a205154df3606f8ac173dd5ceb
thefutureofblockchain <119864425+thefutureofblockchain@users.noreply.github.com>
2023-03-30 17:33:16 +0000

    Thu, Mar 30, 2023, 5:33 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
-
+#include <string.h>
 
 int main(int argc,  string argv[])
 {
@@ -11,4 +11,12 @@ int main(int argc,  string argv[])
     }
     string k = argv[1];
 
-}
+char c;
+
+for (int n = 1; n <= strlen(k); n = n+1) {
+
+        c = k[n-1];
+        if (isdigit(c)) {
+            printf("error\n");
+            return 1;
+        }}}
